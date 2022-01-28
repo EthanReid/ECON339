2ede00057eb3a484cf3ba6b81e5725ed05b41b67
Ethan Reid <erpreid@gmail.com>
2022-01-27 22:11:34 -0800

    Update functionsMisc.R

--- functionsMisc.R
+++ functionsMisc.R
@@ -1,5 +1,6 @@
 #Ethan Reid 2022
 #Misc Functions For ECON339
+#To use add source('<your path>/functionsMisc.R') to the top of your file
 RMSE_339 <- function(vData, pData){
   RMSE <- sqrt(mean((vData-pData)^2))
   return(RMSE)
