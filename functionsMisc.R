b7b466a061fb3cb19a1b5fa1b4b5dd0d257c0eb1
Ethan Reid <erpreid@gmail.com>
2022-01-27 22:02:20 -0800

    R_SQ_339

--- functionsMisc.R
+++ functionsMisc.R
@@ -4,3 +4,9 @@ RMSE_339 <- function(vData, pData){
   RMSE <- sqrt(mean((vData-pData)^2))
   return(RMSE)
 }
+
+R_SQ_339 <- function(model, tData){
+  predModel <- predict(model)
+  R_SQ <- (cor(tData, predModel))^2
+  return(R_SQ)
+}
